9810190126c865fb559fca7859644c65c2a01744
Louise McCloy <lmccloy652@gmail.com>
2020-10-02 17:00:53 +0100

    Adding in UI components to add run details

--- app/src/main/java/com/android/jogon/ui/main/fragments/ResultFragment.kt
+++ app/src/main/java/com/android/jogon/ui/main/fragments/ResultFragment.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.android.jogon.R
+import kotlinx.android.synthetic.main.fragment_result.*
+import java.time.Duration
 
 class ResultFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
-
+        var view =inflater.inflate(R.layout.fragment_result, container, false)
+        return view
 }
 
 }
+
+
